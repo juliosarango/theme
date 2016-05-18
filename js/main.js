3afa04435584c713aca1fa72ffba5a80ba8a1746
Julio Sarango <jsarangoq@gmail.com>
2016-05-18 16:44:53 -0500

    mas cambios

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
+   var nav = $('.navbar-fixed-top');
+   &(window).scroll(function(){
+   	   var scroll = $(window).scrollTop();
+   	   if (scroll)
+
+   });
+
    $('section#pantallas a').on('click',function(){
    	 $('#modal img').attr('src',$(this).attr('data-image-url'));
 
